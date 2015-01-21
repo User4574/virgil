2ed79c336647fd2e9d9527a05c72b4b14ff14598
Nathan Lasseter <Nathan Lasseter nathan@bytemark.co.uk>
2015-01-21 17:49:37 +0000

    Syntax error

--- example.html.rb
+++ example.html.rb
@@ -10,15 +10,15 @@ puts Tag.html {
     Tag.title {
       "My Virgil site"
     }
-  }
+  } +
   Tag.body {
     Tag.h1 {
       "My Virgil site"
-    }
+    } +
     Tag.p ({:class => "italic"}) {
       "some content"
-    }
-    Tag.img ({:src => "img.jpg"})
+    } +
+    Tag.img ({:src => "img.jpg"}) +
     Tag.a ({"href" => "/"}) {
       "A link home"
     }
